3166066bcc8911272ec0025b39b3f2d1321315e3
George BV <egor.workroom@gmail.com>
2019-05-17 17:49:50 +0600

    Add zap-sentry wrapper

--- zap-logger/configure.go
+++ zap-logger/configure.go
@@ -1,8 +1,14 @@
 package logger
 
 import (
+	"os"
+
+	zapsentry "github.com/gebv/go-utils/zap-sentry"
+	"github.com/getsentry/raven-go"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zapgrpc"
+	"google.golang.org/grpc/grpclog"
 )
 
 // Configure configure zap logger.
@@ -19,4 +25,79 @@ func Configure(levelSet string) {
 	}
 	zap.ReplaceGlobals(l)
 	zap.RedirectStdLog(l.Named("stdlog"))
+}
+
+type LoggerConfig struct {
+	SentryDSN               string
+	EnableDevelopmentLogger bool
+	EnableDebugLevelLogger  bool
+	Version                 string
+}
+
+func SetLogger(s *LoggerConfig) func() error {
+	dsn := s.SentryDSN
+	host, err := os.Hostname()
+	if err != nil {
+		zap.L().Panic("Failed to get hostname.", zap.Error(err))
+	}
+
+	if err := raven.SetDSN(dsn); err != nil {
+		zap.L().Panic("Failed to set Sentry DSN.", zap.Error(err))
+	}
+
+	raven.CaptureMessageAndWait("Start application", nil)
+
+	sentryCore, err := zapsentry.Configuration{
+		DSN: dsn,
+		Tags: map[string]string{
+			"host": host,
+		},
+		Release: s.Version,
+	}.Build()
+	if err != nil {
+		zap.L().Panic("Failed to create Sentry logger.", zap.Error(err))
+	}
+
+	// stop using early startup logger
+	zap.L().Sync()
+
+	// create logger config (production or development)
+	var config zap.Config
+	if s.EnableDevelopmentLogger {
+		config = zap.NewDevelopmentConfig()
+		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	} else {
+		config = zap.NewDevelopmentConfig() // replace with zap.NewProductionConfig() when we are ready to use it
+		config.Development = false
+		config.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	}
+	if s.EnableDebugLevelLogger {
+		config.Level.SetLevel(zap.DebugLevel)
+	} else {
+		config.Level.SetLevel(zap.InfoLevel)
+	}
+
+	// create logger from config and Sentry wrapper
+	l, err := config.Build(
+		zap.AddStacktrace(zap.ErrorLevel),
+		zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+			return zapcore.NewTee(core, sentryCore)
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	zap.ReplaceGlobals(l)
+	zap.RedirectStdLog(l.Named("stdlog"))
+
+	grpclog.SetLogger(zapgrpc.NewLogger(l, zapgrpc.WithDebug()))
+
+	if config.Development {
+		zap.L().Info("Development logger initiated.")
+	} else {
+		zap.L().Info("Production logger initiated.")
+	}
+
+	return l.Sync
 }
---
+++ zap-sentry/logger.go
@@ -0,0 +1,105 @@
+package zapsentry
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	Development = "development"
+	Production  = "production"
+	Nop         = "nop"
+)
+
+var logger *zap.SugaredLogger
+
+type Option func(o *option)
+
+type option struct {
+	stage        string
+	sentryDns    string
+	release      string
+	sentryTags   map[string]string
+	sentrtFields []zapcore.Field
+}
+
+func init() {
+	l, _ := zap.NewDevelopment()
+	logger = l.Sugar()
+}
+
+func WithSentry(sentryDNS string, tags map[string]string, fields []zapcore.Field) Option {
+	return func(o *option) {
+		o.sentryDns = sentryDNS
+		o.sentryTags = tags
+		o.sentrtFields = fields
+	}
+}
+
+func WithStage(stage string) Option {
+	return func(o *option) {
+		o.stage = stage
+	}
+}
+
+func WithRelease(release string) Option {
+	return func(o *option) {
+		o.release = release
+	}
+}
+
+func S() *zap.SugaredLogger {
+	return logger
+}
+
+func newzap(stage string) *zap.Logger {
+	var l *zap.Logger
+	switch stage {
+	case Production:
+		l, _ = zap.NewProduction()
+	case Nop:
+		l = zap.NewNop()
+	default:
+		l, _ = zap.NewDevelopment()
+	}
+
+	return l
+}
+
+func createLoagger(o *option) *zap.Logger {
+	l := newzap(o.stage)
+	if o.sentryDns == "" || o.sentryDns == "test" {
+		return l
+	}
+
+	cfg := Configuration{
+		DSN:     o.sentryDns,
+		Tags:    o.sentryTags,
+		Release: o.release,
+	}
+
+	sentryCore, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	if o.sentrtFields != nil && len(o.sentrtFields) > 0 {
+		sentryCore = sentryCore.With(o.sentrtFields)
+	}
+
+	return l.WithOptions(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+		return zapcore.NewTee(core, sentryCore)
+	}))
+}
+
+func NewDefault(opts ...Option) {
+	logger = New(opts...)
+}
+
+func New(opts ...Option) *zap.SugaredLogger {
+	o := &option{}
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	return createLoagger(o).Sugar()
+}
